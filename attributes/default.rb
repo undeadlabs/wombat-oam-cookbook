43149268a59fa78aa285813811e15b31e2803e88
Jamie Winsor <jamie@vialstudios.com>
2014-08-06 17:12:22 -0700

    allow inet listen min/max to be configured by cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,6 +18,9 @@ default[:wombat_oam][:erlang_cookie] = "wombat"
 default[:wombat_oam][:otp_name]      = "wombat@#{node[:fqdn]}"
 default[:wombat_oam][:home]          = "/home/wombat"
 
+default[:wombat_oam][:kernel][:inet_dist_listen_min] = 10000
+default[:wombat_oam][:kernel][:inet_dist_listen_max] = 50000
+
 #
 # Private
 #
