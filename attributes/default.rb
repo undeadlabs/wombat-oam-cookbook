0e7268705aadd83bd2d8f41f254348718df8e60b
Jamie Winsor <jamie@vialstudios.com>
2014-08-07 14:01:35 -0700

    add nginx proxy to cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,6 +17,7 @@ default[:wombat_oam][:log_dir]       = "/var/log/wombat-oam"
 default[:wombat_oam][:erlang_cookie] = "wombat"
 default[:wombat_oam][:otp_name]      = "wombat@#{node[:fqdn]}"
 default[:wombat_oam][:home]          = "/home/wombat"
+default[:wombat_oam][:host]          = node[:fqdn]
 
 default[:wombat_oam][:kernel][:inet_dist_listen_min] = 10000
 default[:wombat_oam][:kernel][:inet_dist_listen_max] = 50000
